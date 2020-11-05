f3f1cdd29ab3f942d96f703337ffc907ff016627
Gagan0504 <71992834+Gagan0504@users.noreply.github.com>
2020-11-05 18:28:27 -0500

    Add files via upload

--- lab5q1.php
+++ lab5q1.php
@@ -4,33 +4,25 @@
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
-
 .button {
-    	border-radius: 15px;
-     	font-size: 30px;
-     	text-align: left;
-     	padding: 15px 20px;
-     	cursor: pointer;
+    	border-radius: 20px;
+     	font-size: 20px;
+     	text-align: center;
 		background-color: red;
 	color: white;
 }
-
 div{
 	height: auto;
      	width: 500px;
-     	margin: 30px auto;
-     	background-color: black;
+     	margin: 10px auto;
+     	background: pink;
      	padding: 20px;
 }
 
 
 html, body {
-	background-color: rgb(28, 231, 221);
-    transition: 0.8s;
-    -webkit-transition: 0.8s;
-    -moz-transition: 0.8s;
-    -ms-transition: 0.8s;
-    -o-transition: 0.8s;
+	font-size: 25px;
+	text-align: center;
 }
 </style>
 </head>
@@ -38,44 +30,44 @@ html, body {
 <div>
 <?php
 session_start();
-if (isset($_SESSION['view']))
-	$_SESSION['view'] = $_SESSION['view'] + 1;
+if (isset($_SESSION['views']))
+	$_SESSION['views'] = $_SESSION['views'] + 1;
 else
-	$_SESSION['view'] = 1;
+	$_SESSION['views'] = 1;
 
 
 
 
 
-$rows = $_POST['row'];
-$columns = $_POST['column'];
-$rowNumber = ($rows >= "3" and $rows <= "12");
-$columnNumber = ($columns >= "3" and $columns <= "12");
+$row = $_POST['row'];
+$column = $_POST['column'];
+$rowCheck = ($row >= "3" and $row <= "12");
+$colCheck = ($column >= "3" and $column <= "12");
 
-if ($rowNumber and $columnNumber) {
-	echo "<h2>Multiplication Table </h2>";
-	echo "Views: " . $_SESSION['view'] . "<br><br>";
+if ($rowCheck and $colCheck) {
+	echo "<h2>Multiplication Table Generator</h2>";
+	echo "Views: " . $_SESSION['views'] . "<br><br>";
 	echo "<br><br>";
 	echo "<table align='center'>";
-	for ($i = 1; $i <= $rows; $i++) {
+	for ($i = 1; $i <= $row; $i++) {
 		echo "<tr>";
-		for ($j = 1; $j <= $columns; $j++) {
+		for ($j = 1; $j <= $column; $j++) {
 			echo "<td>" . $i*$j . "</td>";
 		}
 		echo "</tr>";
 	}
 	echo "</table>";
 	echo "<br>";
-	
+	echo"<p>Here is your table. $5 please.</p>";
 }
 else {
-	echo "<h2>Multiplication Table</h2>";
-	echo "Views: " . $_SESSION['view'] . "<br><br>";
+	echo "<h2>Multiplication Table Generator</h2>";
+	echo "Views: " . $_SESSION['views'] . "<br><br>";
 	echo "<br>";
-	echo "<h4>Please enter the required number of row and columns</h4>";
-	echo "<p>They must be between 3 and 12 </p>";
+	echo "<h4>Rows and column does not meet the requirement.</h4>";
+	echo "<p>Rows and columns must be between 3-12.</p>";
 }
-echo "<button onclick=\"window.location.href = 'lab5q1.html'\">Another Table </button>";
+echo "<button onclick=\"window.location.href = 'lab5Q1.html'\">Create more Tables </button>";
 echo "<br><br>";
 ?>
 </div>
